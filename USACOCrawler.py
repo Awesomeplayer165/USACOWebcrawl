dee8efe835245b154ef37301a47bcb6044f8010b
Awesomeplayer165 <dev@jacob2.dev>
2024-05-10 09:39:06 -0700

    switch to json for better maintainability/flexibility

--- USACOCrawler.py
+++ USACOCrawler.py
@@ -1,26 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
+base_url = 'https://usaco.org'
 class USACOCrawler:
-    base_url = 'https://usaco.org'
 
     @staticmethod
     def get_contest_links() -> list:
-        website_text = requests.get(f'{USACOCrawler.base_url}/index.php?page=contests').text
+        website_text = requests.get(f'{base_url}/index.php?page=contests').text
         soup = BeautifulSoup(website_text, 'html.parser')
         allResults = soup.find_all(lambda tag: tag.name == 'a' and tag.get('href').startswith('index.php?page=') and tag.get('href').endswith('results'))
         return [link.get('href') for link in allResults]
 
     @staticmethod
     def get_contest_problems(contest_link: str) -> list:
-        website_text = requests.get(f'{USACOCrawler.base_url}/{contest_link}').text
+        website_text = requests.get(f'{base_url}/{contest_link}').text
         soup = BeautifulSoup(website_text, 'html.parser')
         contest_problems = soup.find_all(lambda tag: tag.name == 'a' and tag.get('href').startswith('index.php?page=viewproblem2'))
         return [problem.get('href') for problem in contest_problems]
 
     @staticmethod
     def get_contest_problem(contest_problem_link: str) -> str:
-        website_text = requests.get(f'{USACOCrawler.base_url}/{contest_problem_link}').text
+        website_text = requests.get(f'{base_url}/{contest_problem_link}').text
         soup = BeautifulSoup(website_text, 'html.parser')
         return USACOCrawler._clean_contest_problem(soup.find('div', {'class': 'problem-text'}).text)
 
@@ -31,28 +32,42 @@ class USACOCrawler:
         contest_problem_text = contest_problem_text.replace('\le', '<')
         contest_problem_text = contest_problem_text.replace('\ge', '>')
         contest_problem_text = contest_problem_text.replace('$', '')
+        contest_problem_text = contest_problem_text.replace(',', '')
+        contest_problem_text = contest_problem_text.replace('\n', ' ')
         return contest_problem_text
 
-class CSVHelper:
+class FileHelper:
     file_name: str
+    file_contents: dict = {} # memory buffer of file content, updated privately
 
     def __init__(self, file_name: str):
         self.file_name = file_name
+        self.file_contents = self._read_problems()
 
-    def write_to_csv(self, problem_text: str, problem_link: str):
-        with open(self.file_name, 'a') as file:
-            file.write(f'{problem_link},{problem_text}\n')
+    def append_problem_to_file(self, problem_text: str, problem_link: str):
+        with open(self.file_name, 'w') as file:
+            self.file_contents[problem_link] = problem_text
+            file.write(json.dumps(self.file_contents))
+            print(f"Successfully wrote {problem_link}")
+
+    def _read_problems(self) -> dict:
+        with open(self.file_name, 'r') as file:
+            return json.loads(file.read()) or {}
 
 # Crawler Example Usage
-
-
+#"""
 contests = USACOCrawler.get_contest_links()
-print(contests)
+#print(contests)
 contest_problems = USACOCrawler.get_contest_problems(contests[0])
-print(contest_problems[0])
+#print(contest_problems[0])
 problem = USACOCrawler.get_contest_problem(contest_problems[0])
-print(problem)
+problem_two = USACOCrawler.get_contest_problem(contest_problems[1])
+#print(problem)
 
 # CSV Example Usage
-
-CSVHelper("/Users/jacobtrentini/Development/USACOWebcrawl/usaco_problems.csv").write_to_csv(problem, contest_problems[0])
+#"""
+fileHelper = FileHelper("/Users/jacobtrentini/Development/USACOWebcrawl/usaco_problems.json")
+print(fileHelper.file_contents)
+#fileHelper.append_problem_to_file(problem, contest_problems[0])
+#fileHelper.append_problem_to_file(problem_two, contest_problems[1])
+#FileHelper("/Users/jacobtrentini/Development/USACOWebcrawl/usaco_problems.json").append_problem_to_file(problem, contest_problems[0])
