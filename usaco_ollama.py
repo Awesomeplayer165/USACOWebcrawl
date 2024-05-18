5c18caa42ae3bd4350e8edfbf479f135eb53d2c3
Awesomeplayer165 <dev@jacob2.dev>
2024-05-18 09:47:21 -0700

    code gen works, need output

--- usaco_ollama.py
+++ usaco_ollama.py
@@ -1,33 +1,56 @@
 import ollama
 import csv
 from datetime import datetime
+import csv
+import json
 
-sourceFile = open('/Users/jacobtrentini/Development/USACOWebcrawl/usaco_ollama_out.txt', 'w')
+allJson = {}
 
-with open('/Users/jacobtrentini/Development/USACOWebcrawl/usaco_problems_phase1.csv', mode ='r') as file:
-    count = 0
-    csvFile = csv.reader(file)
-    for lines in csvFile:
-        if count == 0:
-            count += 1
-            continue;
-        message = lines[1] + '. do NOT provide any explanation EXCEPT the code formatted in markdown without noting the language. The code generated MUST be in python.'
-        startTime = datetime.now()
-        stream = ollama.chat(
+def generate_code(): 
+    global allJson
+    sourceFile = open('/Users/jacobtrentini/Development/USACOWebcrawl/usaco_ollama_out.json', 'w')
+    with open('/Users/jacobtrentini/Development/USACOWebcrawl/usaco_problems_phase1.csv', mode ='r') as file:
+        count = 0
+        csvFile = csv.reader(file)
+        for lines in csvFile:
+            if count == 0:
+                count += 1
+                continue;
+            message = lines[1] + '. do NOT provide any explanation EXCEPT the code formatted in markdown without noting the language. The code generated MUST be in python.'
+            startTime = datetime.now()
+            response = ollama.chat(
                 model='llama3',
-            messages=[{'role': 'user', 'content': message}],
-            stream=True,
-        )
-        print(lines[0], file = sourceFile)
-        print("\n", file = sourceFile)
-        for chunk in stream:
-            print(chunk['message']['content'], end='', flush=True, file = sourceFile)
-        endTime = datetime.now()
-        timeSpent = (endTime - startTime).total_seconds()
-        print(f'Problem {count}: Time Spent Generating: {timeSpent} seconds')
-        print(f"\ntime generating: {timeSpent}s", file = sourceFile) 
-        count += 1
-        print(file = sourceFile)
-        print("------------------------------------------------", file = sourceFile)
+                messages=[{'role': 'user', 'content': message}],
+            )
+            endTime = datetime.now()
+            timeSpent = (endTime - startTime).total_seconds()
+            print(f'Problem {count} on {lines[0]}: Time Spent Generating: {timeSpent} seconds')
+            count += 1
 
-sourceFile.close()
+            code = response['message']['content']
+            code.replace('\n', '')
+            code.replace('"', '')
+            code.replace("'", '')
+
+            append_to_json(lines[0], {
+                "code": code,
+                "time_gen": f'\ntime generating: {timeSpent}s'
+            })
+
+    sourceFile.close()
+
+def append_to_json(key, value: dict):
+    with open('/Users/jacobtrentini/Development/USACOWebcrawl/usaco_ollama_out.json', 'w') as file:
+        allJson[key] = value
+        file.write(json.dumps(allJson))
+        
+
+def read_code():
+    with open('/Users/jacobtrentini/Development/USACOWebcrawl/usaco_ollama_out.json', 'r') as file:
+        data = json.load(file)
+        for key in data:
+            print(f'Problem {key}:')
+            print(data[key]['code'])
+            print(data[key]['time_gen'])
+#generate_code()
+read_code()
